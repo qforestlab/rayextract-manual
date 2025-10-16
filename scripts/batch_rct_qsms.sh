df22f11bfcd813e87c237592fd7ec717309a6027
ZaneTCooper <zanetcooper@gmail.com>
2025-10-16 10:29:39 +0200

    Update batch_rct_qsms.sh
    -Reorganization of file structure to improve general usability
    - Now designed to work with file2ply.py instead of obsolete txtlaslaz2ply.py

--- scripts/batch_rct_qsms.sh
+++ scripts/batch_rct_qsms.sh
@@ -2,10 +2,12 @@
 rm trees/*_raycloud*
 
 echo "Converting all trees in directory to ply via txtlaslaz2ply.py..."
-python txtlaslaz2ply.py trees trees
+python file2ply.py trees trees
+
+cd trees
 
 echo "Running rayimport..."
-for file in ./trees/*.ply; do
+for file in ./*.ply; do
   if [ -f "$file" ]; then
     echo "Processing $file"
     # Add your commands here
@@ -13,25 +15,31 @@ for file in ./trees/*.ply; do
   fi
 done
 
+rm -r rayclouds
+mkdir rayclouds
+mv *_raycloud.ply rayclouds
+
 echo "Running rayextract terrain..."
-for file in ./trees/*_raycloud.ply; do
+for file in ./rayclouds/*_raycloud.ply; do
   if [ -f "$file" ]; then
     echo "Processing $file"
     # Add your commands here
     rayextract terrain "$file"
   fi
 done
-mkdir 
+
+rm -r terrain_meshs
+mkdir terrain_meshs
+mv ./rayclouds/*_raycloud_mesh.ply ./terrain_meshs
 
 echo "Running rayextract trees..."
-data_folder="./trees"
 
-for data_file in "$data_folder"/*_raycloud.ply; do
+for data_file in ./rayclouds/*_raycloud.ply; do
   # Extract the basename without '_raycloud.ply'
   base_name=$(basename "$data_file" | sed 's/_raycloud\.ply//')
 
   # Find the corresponding mesh file
-  mesh_file="$data_folder/${base_name}_raycloud_mesh.ply"
+  mesh_file="./terrain_meshs/${base_name}_raycloud_mesh.ply"
   
   rayextract trees "$data_file" "$mesh_file" --max_diameter 2 --distance_limit 9
 #  if [ -f "$mesh_file" ]; then
@@ -43,8 +51,18 @@ for data_file in "$data_folder"/*_raycloud.ply; do
 #  fi
 done
 
+rm -r qsms
+mkdir qsms
+mv ./rayclouds/*_trees.txt ./qsms
+rm -r trees_mesh
+mkdir trees_mesh
+mv ./rayclouds/*_trees_mesh.ply ./trees_mesh
+rm -r segmented_pcs
+mkdir segmented_pcs
+mv ./rayclouds/*_segmented.ply ./segmented_pcs
+
 echo "Running batch_tree_info.sh"
-for file in ./trees/*trees.txt; do
+for file in ./qsms/*trees.txt; do
   if [ -f "$file" ]; then
     echo "Processing $file"
     # Add your commands here
@@ -52,24 +70,10 @@ for file in ./trees/*trees.txt; do
  fi
 done
 
-cd trees
-rm -r rayclouds
-mkdir rayclouds
-mv *_raycloud.ply rayclouds
-rm -r terrain_meshs
-mkdir terrain_meshs
-mv *_raycloud_mesh.ply terrain_meshs
-rm -r qsms
-mkdir qsms
-mv *_trees.txt qsms
 rm -r trees_info
 mkdir trees_info
-mv *_trees_info.txt trees_info
-rm -r trees_mesh
-mkdir trees_mesh
-mv *_trees_mesh.ply trees_mesh
-rm *_segmented.ply
+mv ./qsms/*_trees_info.txt ./trees_info
+
 cd ..
 
 echo "All scripts have been executed!"
-echo "Note: All '_segemented.ply' files removed (they are duplicates of input rayclouds)" 
